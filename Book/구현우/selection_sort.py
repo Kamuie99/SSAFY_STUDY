d4adf8037648fe97bfeda54e1789fde3936d1c0e
구현우 <kjkkovekhw@naver.com>
2024-02-04 13:41:35 +0900

    현우_선택정렬

---
+++ Book/구현우/selection_sort.py
@@ -0,0 +1,25 @@
+# selection_sort
+
+# 버블정렬이랑 무슨차이 인지 ?
+# 가장 작은 데이터를 선택해 맨 앞에 있는 데이터와 바꾸고
+# 그 다음 작은 데이터를 선택해 앞에서 두 번째 데이터와 바꾸는 과정을 반복
+
+array = [7,5,9,0,3,1,6,2,4,8]
+
+for i in range(len(array)):
+    min_index = i
+    for j in range(i + 1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i] , array[min_index] = array[min_index] , array[i]
+
+print(array)
+
+for i in range(len(array)):
+    min_index = i
+    for j in range(i + 1, len(array)):
+        if array[min_index] < array[j]:
+            min_index = j
+    array[i] , array[min_index] = array[min_index] , array[i]
+
+print(array)
