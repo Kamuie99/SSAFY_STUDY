bbe692a1f08c16a3d8903329e6bef6b305e769bb
구현우 <kjkkovekhw@naver.com>
2024-02-12 21:46:55 +0900

    20240212 study

--- Book/구현우/05_DFS_BFS/음료수 얼려 먹기/음료수 얼려 먹기.py
+++ Book/구현우/05_DFS_BFS/음료수 얼려 먹기/음료수 얼려 먹기.py
@@ -1,6 +1,7 @@
 import sys
 sys.stdin = open('input.txt')
 
+
 # N, M을 공백을 기준으로 구분하여 입력 받기
 n, m = map(int, input().split())
 
@@ -17,17 +18,18 @@ visited = [[False] * m for _ in range(n)]
 # 모든 상태를 False로 지정
 # 방문한 노드를 표시하기 위해 visited 만들어줌
 
-# 상, 하, 좌, 우로 이동하는 함수
+# 상, 하, 좌, 우로 이동
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 stack = []
 result = 0
+# 아이스크림 만들어지는 갯수 결과를 result라는 변수에 저장해줄거임.
 
 for i in range(n):
     for j in range(m):
         # 방문하지 않은 노드이고, 얼음이 있는 경우
-        if not visited[i][j] and graph[i][j] == 0:
+        if visited[i][j] == False and graph[i][j] == 0:
             stack.append((i, j))
             result += 1
             visited[i][j] = True
@@ -40,9 +42,11 @@ for i in range(n):
                 for k in range(4):
                     nx = x + dx[k]
                     ny = y + dy[k]
-                    # 다음 위치가 범위 내에 있으며, 방문하지 않았고,
-                    # 또 얼음이 있는 경우에만 한다.
+                    # 현재 위치에서 상하좌우로 이동했을떄 위치를 nx , ny 에 넣어줌
+
                     if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and graph[nx][ny] == 0:
+                    # 다음 위치가 범위 내에 있으며, 방문하지 않았고,
+                    # 또 얼음이 있는 경우에만 한다.  
                         stack.append((nx, ny))
                         # 위치를 또 stack에 추가하고
                         visited[nx][ny] = True
