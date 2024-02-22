413caf14f4a35992d38922a896163b9d80701691
구현우 <kjklovekhw@gmail.com>
2024-02-22 22:09:53 +0900

    구현우 시험대비풀이

--- 03_시험대비/01_각자_풀이/구현우/이코테05_DFS_BFS_미로_탈출/solution.py
+++ 03_시험대비/01_각자_풀이/구현우/이코테05_DFS_BFS_미로_탈출/solution.py
@@ -1,2 +1,22 @@
 import sys
+from collections import deque
 sys.stdin = open('input.txt')
+
+def bfs():
+    queue = deque([[0, 0]])
+
+    while queue:
+        x, y = queue.popleft()
+        for dx, dy in (-1, 0), (1, 0), (0, -1), (0, 1):
+            nx, ny = x + dx, y + dy
+            if (0 <= nx < n) and (0 <= ny < m) and graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append([nx, ny])
+
+    return graph[n - 1][m - 1]
+
+
+n, m = map(int, input().split())
+graph = [list(map(int, input().rstrip())) for _ in range(n)]
+
+print(bfs())
